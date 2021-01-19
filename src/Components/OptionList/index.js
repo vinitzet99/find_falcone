dfcc26a5139019f88fd7303770b2764a7f5236cf
vinit <vinitsxn@gmail.com>
2021-01-19 15:14:32 +0530

    optionList edit 1

--- src/Components/OptionList/index.js
+++ src/Components/OptionList/index.js
@@ -7,11 +7,7 @@ import axios from "axios";
 function OptionList() {
   let [planetList, setPlanetList] = useState([]);
   let [vehicleList, setVehicleList] = useState([]);
-  let [show, updateShow] = useState(0);
   let selected = { planet_names: [], vehicle_names: [] };
-  const x = [0, 1, 2, 3];
-  let value = 0;
-  let prevvalue = 0;
 
   useEffect(
     () =>
@@ -39,49 +35,19 @@ function OptionList() {
     []
   );
 
-  useEffect(()=>{console.log(show)},[value]);
   function handleChange(planet, vehicle) {
     console.log("called handleChhage");
     selected.planet_names.push(planetList[planet].name);
     selected.vehicle_names.push(vehicleList[vehicle].name);
-    console.log(value);
   }
   return (
     <>
-      {show > 0 && <ShowSelected list={selected} />}
       <div className="displayContainer">
-        {(value += 1)}
-        {show == 0 && (
-          <>
-            <Option
-              planet={planetList}
-              vehicle={vehicleList}
-              handleChange={handleChange}
-            />
-            {console.log("show" + show)}
-          </>
-        )}
-        {show == 1 && (
-          <Option
-            planet={planetList}
-            vehicle={vehicleList}
-            handleChange={handleChange}
-          />
-        )}
-        {show == 2 && (
-          <Option
-            planet={planetList}
-            vehicle={vehicleList}
-            handleChange={handleChange}
-          />
-        )}
-        {show == 3 && (
-          <Option
-            planet={planetList}
-            vehicle={vehicleList}
-            handleChange={handleChange}
-          />
-        )}
+        <Option
+          planet={planetList}
+          vehicle={vehicleList}
+          handleChange={handleChange}
+        />
       </div>
     </>
   );
